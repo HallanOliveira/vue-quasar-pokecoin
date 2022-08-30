6b9a0926f644d5b4d8f11b863fb481ce66107b7c
Hallan Silva <hallan_douglas@hotmail.com>
2022-08-30 17:48:07 -0300

    partial

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -2,22 +2,55 @@
   <q-page>
     <div class="title-wallet text-primary"><i>Carteira</i></div>
     <q-separator />
-    <div>
+    <div class="row">
+      <div class="col-9">
+
+      </div>
+      <div class="price col-3">
+        Preço atual Bitcoin:<br/>$ {{bitcoinPrice}}
+      </div>
     </div>
   </q-page>
 </template>
 
 <script>
 import { defineComponent } from 'vue'
+import axios from 'axios'
 
 export default defineComponent({
   name: 'IndexPage',
   data () {
     return {
-      data: null
+      bitcoinPrice: null,
+      pokeCoinPrice: null,
+      myPokemons: null
     }
   },
-  methods: {}
+  methods: {
+    calculatePokeCoin (base_experience) {
+      if (base_experience) {
+        var unitPokePrice = this.bitcoinPrice * 0.00000001
+        return unitPokePrice * base_experience
+      }
+      return null
+    },
+    getBitcoinPrice () {
+      axios.get('https://blockchain.info/ticker').then((data) => {
+        this.bitcoinPrice = data.data.USD.last
+      })
+    },
+    getPokemons () {
+      var stringPokemons = localStorage.getItem('Pokemons')
+      if (stringPokemons) {
+        return JSON.parse(stringPokemons)
+      }
+      return null
+    }
+  },
+  created () {
+    this.getBitcoinPrice()
+    this.getPokemons()
+  }
 })
 </script>
 
@@ -28,5 +61,11 @@ export default defineComponent({
     padding: 10px;
     width: 100%;
     text-align: center;
+  }
+
+  .price {
+    font-size: 20pt;
+    text-align: right;
+    padding: 10px
   }
 </style>
